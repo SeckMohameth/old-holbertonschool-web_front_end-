bd52767ed4472923ab463992797de46ee8a45379
SeckMohameth <mohamethseck34@gmail.com>
2020-04-08 17:13:47 +0000

    added file

---
+++ 0x0C-Javascript_advanced/5-mode.js
@@ -0,0 +1,11 @@
+const changeMode = (size, weight, transform, background) => {
+   return (font-size, font-weight, text-transform, background-color, color)
+}
+
+const main = () => {
+    let spooky = changeMode(9, bold, uppercase, pink, green)
+    let darkMode = changeMode(12, bold, capitalize, black, white)
+    let screamMode = changeMode(12, normal, lowercase, white, black)
+}
+
+main()
